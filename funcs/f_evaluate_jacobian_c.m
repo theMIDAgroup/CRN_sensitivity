6d723f7928bfaaa94421f38492f604765ac5fde5
giorgiabiddau <112938151+giorgiabiddau@users.noreply.github.com>
2023-01-11 09:05:01 +0100

    Update f_evaluate_jacobian_c.m

--- funcs/f_evaluate_jacobian_c.m
+++ funcs/f_evaluate_jacobian_c.m
@@ -1,16 +1,21 @@
-%jacobiano di f rspetto a c
 function jacobian = f_evaluate_jacobian_c(n_species, n_idx, lof)
-% ordine=zeros(n_idx,1);
 
-% for j=1:n_idx
-%     ordine(j) = find(Nl(:, idx_basic_species(j)));
-% end
+%% Compute the Jacobian of f with respect to vector c
+
 if lof==0
+
+% Case gof mutation
+
 I_n_idx=eye(n_idx);
-jacobian=[zeros(n_species-n_idx, n_idx); -I_n_idx];%(ordine,:)];
+jacobian=[zeros(n_species-n_idx, n_idx); -I_n_idx];
+
 else
+
+% Case lof mutation that involves one elemental species
+
 n_idx=n_idx-lof;
 I_n_idx=eye(n_idx);
-jacobian=[zeros(n_species-n_idx, n_idx); -I_n_idx];%(ordine,:)];
+jacobian=[zeros(n_species-n_idx, n_idx); -I_n_idx];
 
+end
 end
