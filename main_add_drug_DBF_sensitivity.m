ed54ed62bf14e9a93c3e0ed78eb1cc20c1f8c655
giorgiabiddau <112938151+giorgiabiddau@users.noreply.github.com>
2023-11-27 18:12:34 +0100

    SSI for mutated CRN + DBF drug

--- main_add_drug_DBF_sensitivity.m
+++ main_add_drug_DBF_sensitivity.m
@@ -45,7 +45,8 @@ ind_one = CMIM.matrix.ind_one;
 max_counter = 500;
 % drug MIM
 init_drug1 = 50;
-
+% type of mutation
+mut_lof=0;
 %% Step 2. Compute physiological equilibrium
 rho=cons_laws*x_0_phys;
 
@@ -83,48 +84,51 @@ n_reactions=numel(k_drug);
 v_drug=CMIM_drug.matrix.v;
 
 % 3.2. Add RAS mutation
+
+MIM_mut=f_compute_eq_mutated_CRN("Ras", CMIM_drug, idx_basic_species_drug, x_0_combo, rate_constants_combo);
+
 % deleting columns from S and entries from v and k refereed to all the reactions ivolved into RAS mutation
-protein_selected="Ras";
-perc=0;
-[~, ind_react_rem] = ...
-    f_define_mutated_condition(protein_selected, ...
-    x_0_combo, rate_constants_combo, CMIM_drug, perc);
-react_rem=find(ind_react_rem==0);
-
-null_species_names=["RP_GAP_Ras_GTP", "RP_G_GABP_GAP_Ras_GTP", ...
-    "RP_ShP_G_GABP_GAP_Ras_GTP", "ERBP_GAP_Ras_GTP", ...
-    "ERBP_G_GABP_GAP_Ras_GTP", "ERBP_ShP_G_GABP_GAP_Ras_GTP", ...
-    "ERB3P_GAP_Ras_GTP", ...
-    "ERB3P_G_GABP_GAP_Ras_GTP", "ERB3P_ShP_G_GABP_GAP_Ras_GTP"];
-
-[~, null_species]=ismember(null_species_names, CMIM_drug.species.names);
-
-S_drug(:,react_rem)=[];
-k_drug(react_rem)=[];
-n_react_drug=n_reactions-numel(react_rem);
-% check react_rem is in order in v!
-%[~,react_to_rem]=ismember(react_rem,v_drug(:,1));
-v_drug(react_rem,:)=[];
-first_column=v_drug(:,1);
-first_reagent=v_drug(:,2);
-second_reagent=v_drug(:,3);
-idx_reactions=setdiff(1:n_reactions, react_rem);
-
-for ir=1:n_react_drug
-    disp('idx old')
-    disp(CMIM_drug.matrix.v(ir,1))
-    disp(v_drug(ir,1))
-    disp(find(v_drug(ir,1)==idx_reactions))
-    first_column(ir)=find(v_drug(ir,1)==idx_reactions);
-end
-
-v_drug(:,1)=first_column;
-
-% 3.3. Compute equilibrium point of the new MIM
-rho_drug=cons_laws_drug*x_0_combo;
-ris=f_NLPC_restart(x_0_combo, k_drug, S_drug, cons_laws_drug, rho_drug,...
-    idx_basic_species_drug, v_drug, ind_one_drug, max_counter, 1);
-x_e_drug_2=ris.x;
+% protein_selected="Ras";
+% perc=0;
+% [~, ind_react_rem] = ...
+%     f_define_mutated_condition(protein_selected, ...
+%     x_0_combo, rate_constants_combo, CMIM_drug, perc);
+% react_rem=find(ind_react_rem==0);
+% 
+% null_species_names=["RP_GAP_Ras_GTP", "RP_G_GABP_GAP_Ras_GTP", ...
+%     "RP_ShP_G_GABP_GAP_Ras_GTP", "ERBP_GAP_Ras_GTP", ...
+%     "ERBP_G_GABP_GAP_Ras_GTP", "ERBP_ShP_G_GABP_GAP_Ras_GTP", ...
+%     "ERB3P_GAP_Ras_GTP", ...
+%     "ERB3P_G_GABP_GAP_Ras_GTP", "ERB3P_ShP_G_GABP_GAP_Ras_GTP"];
+% 
+% [~, null_species]=ismember(null_species_names, CMIM_drug.species.names);
+% 
+% S_drug(:,react_rem)=[];
+% k_drug(react_rem)=[];
+% n_react_drug=n_reactions-numel(react_rem);
+% % check react_rem is in order in v!
+% %[~,react_to_rem]=ismember(react_rem,v_drug(:,1));
+% v_drug(react_rem,:)=[];
+% first_column=v_drug(:,1);
+% first_reagent=v_drug(:,2);
+% second_reagent=v_drug(:,3);
+% idx_reactions=setdiff(1:n_reactions, react_rem);
+% 
+% for ir=1:n_react_drug
+%     disp('idx old')
+%     disp(CMIM_drug.matrix.v(ir,1))
+%     disp(v_drug(ir,1))
+%     disp(find(v_drug(ir,1)==idx_reactions))
+%     first_column(ir)=find(v_drug(ir,1)==idx_reactions);
+% end
+% % 
+% % v_drug(:,1)=first_column;
+% 
+% % 3.3. Compute equilibrium point of the new MIM
+% rho_drug=cons_laws_drug*x_0_combo;
+% ris=f_NLPC_restart(x_0_combo, k_drug, S_drug, cons_laws_drug, rho_drug,...
+%     idx_basic_species_drug, v_drug, ind_one_drug, max_counter, 1);
+% x_e_drug_2=ris.x;
 
 % 3.4. COmparison with older results
 % rate_constants_combo(react_rem)=0;
@@ -136,76 +140,49 @@ x_e_drug_2=ris.x;
 % plot(sort_xe)
 
 %% Step 4. SSI computation
-
+x_e_drug_2=MIM_mut.species.x_eq;
+null_species=MIM_mut.species.null_species;
 x_e_drug_2(null_species)=0;
-n_basic_species_drug=numel(idx_basic_species_drug);
+idx_sp=find(x_e_drug_2);
 
-% 1. Compute the analic jacobian of v
-jacobian_v = f_compute_analytic_jacobian_v(v_drug, n_species_drug, ind_one_drug);
+k_mut_drug=MIM_mut.rates.std_values;
+S_mut_drug=MIM_mut.matrix.S;
+cons_laws_mut_drug=MIM_mut.matrix.Nl;
+idx_basic_species_mut_drug=MIM_mut.species.idx_basic_species;
+v_mut_drug=MIM_mut.matrix.v;
+rho_mut_drug=cons_laws_mut_drug*x_0_combo;
 
-% 2. Evaluate the Jacobian in x of f
-eval_jac = f_evaluate_jacobian_neworder(k_drug, x_e_drug_2, ...
-    S_drug, idx_basic_species_drug, jacobian_v, cons_laws_drug);
-
-eval_jac_k=f_evaluate_jacobian_k(v_drug, x_e_drug_2, S_drug, idx_basic_species_drug);
-eval_jac_c=f_evaluate_jacobian_c(n_species+n_new_species1, n_basic_species_drug, 0);
-
-% 3. Calcolate Jac_k_xeq e Jac_c_eq
-Jac_k_xeq= -inv(eval_jac)*eval_jac_k;
-Jac_c_xeq= -inv(eval_jac)*eval_jac_c;
-
-x_values=x_e_drug_2;
-x_values(null_species)=1;
-L_k=(1./x_values).*(Jac_k_xeq).*(k_drug');
-L_c=(1./x_values).*(Jac_c_xeq).*(rho_drug');
-
-[U_k,D_k,V_k]=svd(L_k);
-e_PCA_k =  ((V_k(:,1:n_species_drug).^2)*(diag(D_k).^2))/sum(diag(D_k).^2); %se presi con il quadrato: e_j=(L^T*L)_{jj}/sum_k(lambda_k)
-[U_c,D_c,V_c]=svd(L_c);
-e_PCA_c =((V_c(:,1:n_basic_species_drug).^2)*(diag(D_c).^2))/sum(diag(D_c).^2); %se presi con il quadrato: e_j=(L^T*L)_{jj}/sum_k(lambda_k)
+[SSI_k, SSI_c]=f_compute_SSI(idx_sp, x_e_drug_2, k_mut_drug,...
+                                    S_mut_drug, cons_laws_mut_drug, rho_mut_drug,...
+                                    idx_basic_species_mut_drug, v_mut_drug);
 
 %% Step 5. Figure
 f_ssi_phys_vs_drug = figure('units','normalized','outerposition',[0 0  0.75 1]);
 
 % compute physiological SSI
+react_rem=MIM_mut.info.react_rem;
+
 x_values=x_eq_phys;
+idx_sp_phys=1:numel(x_values);
+rho_phys=cons_laws*x_0_phys;
+[SSI_k_phys, SSI_c_phys]=f_compute_SSI(idx_sp_phys, x_values, rate_constants_phys, ...
+                                           Sm, cons_laws, rho_phys, idx_basic_species, vm);
 
-jacobian_v_phys = f_compute_analytic_jacobian_v(vm, n_species, ind_one);
-
-eval_jac_phys = f_evaluate_jacobian_neworder(rate_constants_phys, x_values, ...
-    Sm, idx_basic_species, jacobian_v_phys, cons_laws);
-
-eval_jac_k_phys=f_evaluate_jacobian_k(vm, x_values, Sm, idx_basic_species);
-eval_jac_c_phys=f_evaluate_jacobian_c(n_species, n_cons_laws,0);
-
-Jac_k_xeq_phys= -inv(eval_jac_phys)*eval_jac_k_phys;
-Jac_c_xeq_phys= -inv(eval_jac_phys)*eval_jac_c_phys;
-
-c=cons_laws*x_0_phys;
-L_c_phys=(1./x_values).*(Jac_c_xeq_phys).*(c');
-[U_c_phys,D_c_phys,V_c_phys]=svd(L_c_phys);
-e_PCA_c_phys =  ((V_c_phys(:,1:n_cons_laws).^2)*(diag(D_c_phys).^2))/sum(diag(D_c_phys).^2); %se presi con il quadrato: e_j=(L^T*L)_{jj}/sum_k(lambda_k)
-
-L_k_phys=(1./x_values).*(Jac_k_xeq_phys).*(rate_constants_phys');
-
-[U_k_phys,D_k_phys,V_k_phys]=svd(L_k_phys);
-e_PCA_k_phys =  ((V_k_phys(:,1:n_species).^2)*(diag(D_k_phys).^2))/sum(diag(D_k_phys).^2); %se presi con il quadrato: e_j=(L^T*L)_{jj}/sum_k(lambda_k)
-
-e_PCA_k_phys(react_rem)=[];
+SSI_k_phys(react_rem)=[];
 
 % figures
 
-[aux_PCA_k_phys_sort, order_phys]=sort(e_PCA_k_phys, 'descend');
+[aux_PCA_k_phys_sort, order_phys]=sort(SSI_k_phys, 'descend');
 
 subplot(1,2,1)
-semilogy(e_PCA_k(order_phys), 'k', 'linewidth', 3)
+semilogy(SSI_k(order_phys), 'k', 'linewidth', 3)
 hold on
 semilogy(aux_PCA_k_phys_sort, 'r--', 'linewidth', 3)
 ylim([10^-16, 10^0])
 
-[aux_PCA_c_phys_sort, order_phys]=sort(e_PCA_c_phys, 'descend');
+[aux_PCA_c_phys_sort, order_phys]=sort(SSI_c_phys, 'descend');
 subplot(1,2,2)
-semilogy(e_PCA_c(order_phys), 'k', 'linewidth', 3)
+semilogy(SSI_c(order_phys), 'k', 'linewidth', 3)
 hold on
 semilogy([aux_PCA_c_phys_sort;0;0], 'r--', 'linewidth', 3)
 
@@ -225,8 +202,8 @@ for i=1:numel(react2flux)
     end
 end
 
-[max_values_SSI_k, ord_max_SSI_k]=sort(e_PCA_k,'descend');
-[max_values_SSI_c, ord_max_SSI_c]=sort(e_PCA_c,'descend');
+[max_values_SSI_k, ord_max_SSI_k]=sort(SSI_k,'descend');
+[max_values_SSI_c, ord_max_SSI_c]=sort(SSI_c,'descend');
 str=strings(10,2);
 for i=1:10
     str(i,1)=convertCharsToStrings(arrow{react2flux(ord_max_SSI_k(i))});
@@ -244,10 +221,5 @@ T_c=table(str, max_values_SSI_c(1:10), 'VariableNames', {'Basic Species List', '
 selected_proteins = {'ERKPP'};
 [aux_, idx_proteins] = ismember(selected_proteins, CMIM_drug.species.names);
 
-selpart_L = L_c(idx_proteins, :);
-[U, Sigma, V]=svd(selpart_L);
-selection=size(selpart_L,1);
-eing=diag(Sigma(1:selection, 1:selection));
-sum_eig=sum(eing.^2);
-selpart_e_PCA=((V(:,1:selection).^2)*(eing.^2))/sum_eig;
-
+[selpart_SSI_k, selpart_SSI_c]=f_compute_SSI(idx_proteins,  x_e_drug_2, k_mut_drug,...
+                                    S_mut_drug, cons_laws_mut_drug, rho_mut_drug, idx_basic_species_drug, v_mut_drug);
