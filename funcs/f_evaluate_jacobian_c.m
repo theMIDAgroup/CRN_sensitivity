af0cf594a058af00326fd2ede0be2c4a7780e346
giorgiabiddau <112938151+giorgiabiddau@users.noreply.github.com>
2023-12-05 18:04:36 +0100

    updates

--- funcs/f_evaluate_jacobian_c.m
+++ funcs/f_evaluate_jacobian_c.m
@@ -1,21 +1,10 @@
-function jacobian = f_evaluate_jacobian_c(n_species, n_idx, lof)
+function jacobian = f_evaluate_jacobian_c(n_species, n_idx)
 
 %% Compute the Jacobian of f with respect to vector c
-
-if lof==0
 
 % Case gof mutation
 
 I_n_idx=eye(n_idx);
 jacobian=[zeros(n_species-n_idx, n_idx); -I_n_idx];
 
-else
-
-% Case lof mutation that involves one elemental species
-
-n_idx=n_idx-lof;
-I_n_idx=eye(n_idx);
-jacobian=[zeros(n_species-n_idx, n_idx); -I_n_idx];
-
-end
 end
---
+++ funcs/f_evaluate_jacobian_c_old.m
@@ -0,0 +1,21 @@
+function jacobian = f_evaluate_jacobian_c_old(n_species, n_idx, lof)
+
+%% Compute the Jacobian of f with respect to vector c
+
+if lof==0
+
+% Case gof mutation
+
+I_n_idx=eye(n_idx);
+jacobian=[zeros(n_species-n_idx, n_idx); -I_n_idx];
+
+else
+
+% Case lof mutation that involves one elemental species
+
+n_idx=n_idx-lof;
+I_n_idx=eye(n_idx);
+jacobian=[zeros(n_species-n_idx, n_idx); -I_n_idx];
+
+end
+end
