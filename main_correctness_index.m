e3352e876224a8acbb6be816445453775a6ff198
sarasommariva <sommariva@dima.unige.it>
2024-01-11 15:24:46 +0100

    Improved visualization of the result

--- main_correctness_index.m
+++ main_correctness_index.m
@@ -12,6 +12,7 @@ addpath('./funcs')
 
 %% 1. Load structure containing information on species and reactions
 file_crn = fullfile('data', 'CRC_CRN_nodrug_complete.mat');
+folder_figures = './figures';
 load(file_crn)
 
 %% 2. Initial data
@@ -31,16 +32,14 @@ rho=cons_laws*x_0;
 mut_lof=0;
 
 max_counter = 300;
+
 %% 3. Compute the values of the SSI
-
 % 3.1. Compute equilibrium
-
 ris_phys = f_NLPC_restart(x_0, k_values, Sm, cons_laws, rho, idx_basic_species, ...
           vm, ind_one, max_counter, 0);
 x_values = ris_phys.x;
 
-% 3.2. Analytically compute the sensitivity matrix
-% Compute the analytic jacobian of v 
+% 3.2. Compute the SSI
 idx_sp=1:numel(x_values);
 [SSI_k, SSI_c]=f_compute_SSI(idx_sp, x_values, k_values, ...
                                  Sm, cons_laws, rho, idx_basic_species, vm);
@@ -155,34 +154,51 @@ delta_x_eq_mean_c = (x_eq_mean_c - x_eq_mean_c(:, 1)) ./ x_eq_mean_c(:, 1);
 norm_delta_x_mean_c = vecnorm(delta_x_eq_mean_c, 2, 1);
 
 
-%% Fig 2 paper:
+%% Figure on sensitivity analysis of the CRC-CRN
+fprintf('# Rate constant with SSS > 0.001  ---> %d \n', sum(SSI_k > 0.001))
+fprintf('# Rate constant with SSS in [0.001, 10^-4]  ---> %d \n', ...
+    sum((10^-4 < SSI_k & SSI_k< 0.001)))
+fprintf('# Rate constant with SSS < 10^-4  ---> %d \n', sum(10^-4 > SSI_k))
+
+
+fprintf('# c with SSS > 10^-2.5  ---> %d \n', sum(SSI_c > 10^(-2.5)))
+fprintf('# c with SSS < 10^-2.5  ---> %d \n', sum(SSI_c < 10^(-2.5)))
 
 figure_SSI_corrctness=figure('units','normalized','outerposition',[0 0  0.75 1]);
 
 % 1. Histograms
-
 % 1.1 rates
 subplot(2,2,1)
-binedges=-16:1:-1;
-histogram(log10(SSI_k), 'BinEdges', binedges, 'Normalization', 'percentage')
-yticks([0 0.1, 0.20, 0.30, 0.40 0.5])
-yticklabels({'0', '10', '20', '30', '40', '50'})
-xticks([-20 -15 -10 -5 -1])
-xticklabels({'$10^{-20}$', '$10^{-15}$', '$10^{-10}$', '$10^{-5}$', '$10^{-1}$'})
-ylabel('%')
-xlabel('log(e_j^k)')
+binedges=[-Inf, -6:1:-1];
+histogram(log10(SSI_k), 'BinEdges', binedges, 'Normalization', 'count')
+hold on 
+plot(log10(max_e_k), 1, 'k*', 'Markersize', 10, 'Linewidth', 10)
+plot(log10(SSI_k(idx_k_mean)), 1, 'b*', 'Markersize', 10, 'Linewidth', 10)
+plot(-7, 1, 'r*', 'Markersize', 10, 'Linewidth', 10)
+xlim([-7,-1])
+xticks(-6:1:-1)
+xticklabels({'$10^{-6}$', '$10^{-5}$', '$10^{-4}$', '$10^{-3}$', '$10^{-2}$', '$10^{-1}$'})
+set(gca, 'Fontsize', 18)
+ylabel('Counts', 'Fontsize', 22)
+xlabel('$e_j^\mathbf{k}$', 'Interpreter', 'Latex', 'Fontsize', 22)
+
 
 % 1.2 conservation laws
-
 subplot(2,2,2)
-binedges=-3.25:0.2:-1;
-histogram(log10(SSI_c), 'BinEdges', binedges, 'Normalization', 'probability')
-yticks([0, 0.05, 0.1, 0.15, 0.20, 0.25])
-yticklabels({'0', '5', '10', '15', '20', '25'})
-xticks(-3: 1 : -1)
-xticklabels({'$10^{-3}$', '$10^{-2}$', '$10^{-1}$'})
-ylabel('%')
-xlabel('log(e_j^c)')
+binedges=[-Inf,-3.5:0.5:-0.5];
+histogram(log10(SSI_c), 'BinEdges', binedges, 'Normalization', 'count')
+hold on 
+plot(log10(max_e_c), 0, 'k*', 'Markersize', 10, 'Linewidth', 10)
+plot(log10(SSI_c(idx_c_mean)), 0, 'b*', 'Markersize', 10, 'Linewidth', 10)
+plot(log10(min_e_c), 0, 'r*', 'Markersize', 10, 'Linewidth', 10)
+xlim([-3.5,-0.5])
+xticks(-3:0.5:-0.5)
+xticklabels({'$10^{-3}$', '$10^{-2.5}$', '$10^{-2}$', ...
+    '$10^{-1.5}$', '$10^{-1}$', '$10^{-0.5}$'})
+set(gca, 'Fontsize', 18)
+ylabel('Counts', 'Fontsize', 22)
+xlabel('$e_j^\mathbf{c}$', 'Interpreter', 'Latex', 'Fontsize', 22)
+
 
 % Plots
 % 2.1 rates
@@ -192,45 +208,59 @@ hold on
 plot(delta_j, norm_delta_x_mean_k, 'b-*', 'Linewidth', 3)
 hold on
 plot(delta_j, norm_delta_x_low_k, 'r-*', 'Linewidth', 3)
-
-xlabel('$\hat{\Delta} k$', 'Interpreter', 'Latex')
-ylabel('$\sqrt{Q(\hat{\Delta} k)}$', 'Interpreter', 'Latex')
+set(gca, 'Fontsize', 18)
+xlabel('$\hat{\Delta} k$', 'Interpreter', 'Latex', 'Fontsize', 22)
+ylabel('$\sqrt{Q(\hat{\Delta} k)}$', 'Interpreter', 'Latex', 'Fontsize', 22)
 xticks(delta_j);
 my_symlog('y')
-set(gca, 'Fontsize', 10)
 grid on
 
 % 2.2 conservation laws
 subplot(2,2, 4)
 plot(delta_j, norm_delta_x_high_c, 'k-*', 'Linewidth', 3)
 hold on
-plot(delta_j, norm_delta_x_mean_c, 'b-o', 'Linewidth', 3)
+plot(delta_j, norm_delta_x_mean_c, 'b-*', 'Linewidth', 3)
 hold on
-plot(delta_j, norm_delta_x_low_c, 'r-x', 'Linewidth', 3)
+plot(delta_j, norm_delta_x_low_c, 'r-*', 'Linewidth', 3)
 my_symlog('y')
-% set(gca, 'Yscale', 'log')
-xlabel('$\hat{\Delta} c$', 'Interpreter', 'Latex')
-ylabel('$\sqrt{Q(\hat{\Delta} c)}$', 'Interpreter', 'Latex')
-set(gca, 'Fontsize', 10)
+set(gca, 'Fontsize', 18)
+xlabel('$\hat{\Delta} c$', 'Interpreter', 'Latex', 'Fontsize', 22)
+ylabel('$\sqrt{Q(\hat{\Delta} c)}$', 'Interpreter', 'Latex', 'Fontsize', 22)
 xticks(delta_j)
 grid on
-L=legend({'Maximum SSI';'Median SSI';'Minimum SSI'},'Location','NorthOutside', 'Orientation','horizontal');
+L=legend({'Max SSI';'Median SSI';'Min SSI'},'Location','NorthOutside', 'Orientation','horizontal');
 L.Position=[0.5, 0.479, 0.001, 0.001];
-%L.Color='none';
-%L.EdgeColor='none';
-%% Fig 2:
-% Impact of increasing kinetic parameters referred to
-% maximum
-% minimum
-% median
-% SSI on TP53 concentration
+L.FontSize = 20;
 
+saveas(figure_SSI_corrctness, fullfile(folder_figures, 'figure2.png'))
+
+%% Analysis restricted on TP53
+
+%% Step 1. Compute SSI by considering only TP53's concentration
 selected_proteins = {'TP53'};
 [aux_, idx_proteins] = ismember(selected_proteins, CMIM.species.names);
-
 [~, selpart_SSI]=f_compute_SSI(idx_proteins, x_values, k_values, ...
                                  Sm, cons_laws, rho, idx_basic_species, vm, mut_lof);
 
+%% Step 2. Table on the conservation laws' constant                             
+[temp_col, temp_raw] = find(cons_laws(:, idx_basic_species)');
+bsp_names = new_CMIM.species.names(idx_basic_species(temp_col));
+    % First we reorder the elemental species based on the conservation
+    % laws.
+[selpart_SSI_sorted, selpart_SSI_idx] = sort(selpart_SSI, 'descend');
+bsp_names{selpart_SSI_idx};
+
+aux = fullfile(folder_figures, 'SSI_TP53_c.txt');
+fileID = fopen(aux, 'w');
+n_selpart_SSI = numel(find(selpart_SSI >= 0.015));
+for ii = 1:n_selpart_SSI
+    fprintf(fileID, '%1.3f & \\verb| %s |  \\\\ \n', ...
+        selpart_SSI_sorted(ii), ...
+        bsp_names{selpart_SSI_idx(ii)});
+end
+fclose(fileID);   
+
+%% Additional analysis
 [max_e, idx_c_high] = max(selpart_SSI);
 [min_e, idx_c_low] = min(selpart_SSI);
 aux=median(selpart_SSI); idx_c_mean=find(selpart_SSI==aux);
